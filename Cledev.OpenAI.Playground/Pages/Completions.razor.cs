8d48c3859501a4d7f1b130a820b45460f229f09a
lucabriguglia <luca.briguglia@outlook.com>
2023-03-06 19:01:20 +0000

    Add form label component

--- Cledev.OpenAI.Playground/Pages/Completions.razor.cs
+++ Cledev.OpenAI.Playground/Pages/Completions.razor.cs
@@ -51,4 +51,6 @@ public class CompletionsPage : PageComponentBase
 
         IsLoading = false;
     }
+
+    protected string PromptToolTip = "Optional (Defaults to <|endoftext|>). The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays. Note that <|endoftext|> is the document separator that the model sees during training, so if a prompt is not specified the model will generate as if from the beginning of a new document.";
 }
