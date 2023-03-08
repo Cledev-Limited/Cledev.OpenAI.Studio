cde02af65c0b7e95dbfef7c8bdfc026d83e9df82
lucabriguglia <luca.briguglia@outlook.com>
2023-03-08 14:14:01 +0000

    New embeddings page layout

--- Cledev.OpenAI.Playground/Pages/Embeddings.razor.cs
+++ Cledev.OpenAI.Playground/Pages/Embeddings.razor.cs
@@ -34,7 +34,5 @@ public class EmbeddingsPage : PageComponentBase
     protected static class Tooltips
     {
         public static string Input = "Required. Input text to get embeddings for, encoded as a string or array of tokens. To get embeddings for multiple inputs in a single request, pass an array of strings or array of token arrays. Each input must not exceed 8192 tokens in length.";
-        public static string Model = "Required. ID of the model to use. You can use the List models API to see all of your available models, or see our Model overview for descriptions of them.";
-        public static string User = "Optional. A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.";
     }
 }
