a4141ddecd3e1202e1c9d187e78b2b09c4dedca4
lucabriguglia <luca.briguglia@outlook.com>
2023-03-08 13:46:13 +0000

    Edits page new layout

--- Cledev.OpenAI.Playground/Pages/Edits.razor.cs
+++ Cledev.OpenAI.Playground/Pages/Edits.razor.cs
@@ -14,8 +14,9 @@ public class EditsPage : PageComponentBase
     {
         Request = new CreateEditRequest
         {
-            Model = EditsModel.TextDavinciEditV1.ToStringModel(),
-            Instruction = string.Empty
+            Model = EditsModel.CodeDavinciEditV1.ToStringModel(),
+            Instruction = string.Empty,
+            N = 1
         };
 
         EditModels = Enum.GetValues(typeof(EditsModel)).Cast<EditsModel>().Select(x => x.ToStringModel()).ToList();
@@ -32,5 +33,11 @@ public class EditsPage : PageComponentBase
         Error = Response?.Error;
 
         IsLoading = false;
+    }
+
+    protected static class Tooltips
+    {
+        public static string Input = "Optional (Defaults to \"\"). The input text to use as a starting point for the edit.";
+        public static string Instruction = "Required. The instruction that tells the model how to edit the prompt.";
     }
 }
