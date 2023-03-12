ecf6a11431d90318a44586335e8bf8f823ba5350
lucabriguglia <luca.briguglia@outlook.com>
2023-03-12 20:38:43 +0000

    Add submit button tests class with first test

---
+++ Cledev.OpenAI.Playground.Tests/SubmitButtonTests.cs
@@ -0,0 +1,27 @@
+﻿using Bunit;
+using Cledev.OpenAI.Playground.Shared;
+using FluentAssertions;
+using NUnit.Framework;
+using TestContext = Bunit.TestContext;
+
+namespace Cledev.OpenAI.Playground.Tests;
+
+public class SubmitButtonTests : TestContext
+{
+    [Test]
+    public void ShouldRenderText()
+    {
+        var componentParameters = new[]
+        {
+            ComponentParameter.CreateParameter(
+                name: "Text",
+                value: "Create Completion")
+        };
+
+        var component = RenderComponent<SubmitButton>(componentParameters);
+
+        var buttonText = component.Find("button").TextContent;
+
+        buttonText.Should().Be("Create Completion");
+    }
+}
