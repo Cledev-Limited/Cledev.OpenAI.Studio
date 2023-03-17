cfc7995ddcb479adee2ae632f3a164a521ba971e
lucabriguglia <luca.briguglia@outlook.com>
2023-03-17 11:24:48 +0000

    Add date time extensions

---
+++ Cledev.OpenAI.Studio/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Cledev.OpenAI.Studio.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static DateTime ToDateTime(this int unixTimeStamp)
+    {
+        var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+        return dateTime;
+    }
+
+    public static DateTime ToDateTime(this int? unixTimeStamp, int fallBack)
+    {
+        unixTimeStamp ??= fallBack;
+        var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp.Value).ToLocalTime();
+        return dateTime;
+    }
+}
