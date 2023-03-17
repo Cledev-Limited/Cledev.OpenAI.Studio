1fb88c8f7612070d96f5bf134d21248b1dd6d5a9
lucabriguglia <luca.briguglia@outlook.com>
2023-03-17 18:25:18 +0000

    List fine tunes

---
+++ Cledev.OpenAI.Studio.Tests/Components/Pages/FineTunesTests.cs
@@ -0,0 +1,55 @@
+﻿using Bunit;
+using Cledev.OpenAI.Studio.Pages;
+using Cledev.OpenAI.V1.Contracts.FineTunes;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Error = Cledev.OpenAI.V1.Contracts.Error;
+
+namespace Cledev.OpenAI.Studio.Tests.Components.Pages;
+
+public class FineTunesTests : ComponentTestBase
+{
+    [Test]
+    public void GivenAPIReturnsError_ThenErrorMessageIsRendered()
+    {
+        OpenAIClient
+            .Setup(x => x.ListFineTunes(CancellationToken.None))
+            .ReturnsAsync(new ListFineTunesResponse { Error = new Error { Message = "Some Error Message" } });
+
+        var cut = RenderComponent<FineTuning>();
+
+        cut.WaitForState(() => cut.Find("span[id=errorMessage]") is { TextContent: not null });
+
+        cut.Find("span[id=errorMessage]").TextContent.Should().Contain("Some Error Message");
+    }
+
+    [Test]
+    public void GivenAPIReturnsResult_ThenFineTuneIsRendered()
+    {
+        OpenAIClient
+            .Setup(x => x.ListFineTunes(CancellationToken.None))
+            .ReturnsAsync(new ListFineTunesResponse { Data = new List<FineTuneResponse> { new() { Id = "Id1", Model = "BaseModel", FineTunedModel = "FineTunedModel" } }});
+
+        var cut = RenderComponent<FineTuning>();
+
+        cut.WaitForState(() => cut.Find("td[id=fineTune1Id]") is { TextContent: not null });
+
+        cut.Find("td[id=fineTune1Id]").TextContent.Should().Be("Id1");
+    }
+
+    [Test]
+    public void GivenAPIReturnsResult_ThenErrorMessageIsNotRendered()
+    {
+        OpenAIClient
+            .Setup(x => x.ListFineTunes(CancellationToken.None))
+            .ReturnsAsync(new ListFineTunesResponse { Data = new List<FineTuneResponse> { new() { Id = "Id1", Model = "BaseModel", FineTunedModel = "FineTunedModel" } } });
+
+        var cut = RenderComponent<FineTuning>();
+
+        cut.WaitForState(() => cut.Find("td[id=fineTune1Id]") is { TextContent: not null });
+
+        Action act = () => cut.Find("span[id=errorMessage]");
+        act.Should().Throw<ElementNotFoundException>();
+    }
+}
---
+++ Cledev.OpenAI.Studio/Pages/FineTuning.razor.cs
@@ -0,0 +1,87 @@
+﻿using Cledev.OpenAI.V1.Contracts.FineTunes;
+
+namespace Cledev.OpenAI.Studio.Pages;
+
+public class FineTuningPage : PageComponentBase
+{
+    //public UploadFileRequest UploadFileRequest { get; set; } = null!;
+
+    public List<FineTuneResponse> FineTunes { get; set; } = new();
+    //public string? FileIdToDelete { get; set; }
+
+    //public bool IsUploading { get; set; }
+    //protected Error? UploadError { get; set; }
+
+    //public bool IsDeleting { get; set; }
+    //protected Error? DeleteError { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        //UploadFileRequest = new UploadFileRequest
+        //{
+        //    File = new byte[1],
+        //    FileName = "Something",
+        //    Purpose = "fine-tune"
+        //};
+
+        await LoadFineTunes();
+    }
+
+    //public async Task OnInputFileChange(InputFileChangeEventArgs e)
+    //{
+    //    UploadFileRequest.File = await GetFileBytes(e);
+    //    UploadFileRequest.FileName = e.File.Name;
+    //}
+
+    //protected async Task OnSubmitAsync()
+    //{
+    //    IsUploading = true;
+
+    //    var response = await OpenAIClient.UploadFile(UploadFileRequest);
+    //    UploadError = response?.Error;
+    //    if (UploadError is null)
+    //    {
+    //        await JsRuntime.InvokeVoidAsync("toggleModal", "uploadModal");
+    //        await LoadFineTunes();
+    //    }
+
+    //    IsUploading = false;
+    //}
+
+    protected async Task LoadFineTunes()
+    {
+        IsProcessing = true;
+        FineTunes.Clear();
+
+        StateHasChanged();
+
+        var response = await OpenAIClient.ListFineTunes();
+        Error = response?.Error;
+        if (response is not null)
+        {
+            FineTunes.AddRange(response.Data);
+        }
+
+        IsProcessing = false;
+    }
+
+    //protected void SetFileIdToDelete(string fileIdToDelete)
+    //{
+    //    FileIdToDelete = fileIdToDelete;
+    //}
+
+    //protected async Task DeleteFile()
+    //{
+    //    IsDeleting = true;
+
+    //    var deleteFileResponse = await OpenAIClient.DeleteFile(FileIdToDelete!);
+    //    DeleteError = deleteFileResponse?.Error;
+    //    if (DeleteError is null)
+    //    {
+    //        await JsRuntime.InvokeVoidAsync("toggleModal", "deleteModal");
+    //        await LoadFineTunes();
+    //    }
+
+    //    IsDeleting = false;
+    //}
+}
