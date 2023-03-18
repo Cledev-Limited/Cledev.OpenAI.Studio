7166ecd62b8eb42db2761acbe803841d89374efd
lucabriguglia <luca.briguglia@outlook.com>
2023-03-18 13:57:07 +0000

    Add refesh fine-tune list button

---
+++ Cledev.OpenAI.Studio.Tests/Components/Pages/FineTuning/RefreshFineTunesTests.cs
@@ -0,0 +1,25 @@
+﻿using Bunit;
+using Cledev.OpenAI.V1.Contracts.FineTunes;
+using Moq;
+using NUnit.Framework;
+
+namespace Cledev.OpenAI.Studio.Tests.Components.Pages.FineTuning;
+
+public class RefreshFineTunesTests : ComponentTestBase
+{
+    [Test]
+    public void GivenRefreshButtonIsCLicked_ThenAPIIsCalledToGetNewList()
+    {
+        OpenAIClient
+            .Setup(x => x.ListFineTunes(CancellationToken.None))
+            .ReturnsAsync(new ListFineTunesResponse { Data = new List<FineTuneResponse> { new() { Id = "Id1", Model = "BaseModel", FineTunedModel = "FineTunedModel" } } });
+
+        var cut = RenderComponent<Studio.Pages.FineTuning>();
+
+        cut.WaitForState(() => cut.Find("td[id=fineTune1Id]") is { TextContent: not null });
+
+        cut.Find("button[id=RefreshList]").Click();
+
+        OpenAIClient.Verify(client => client.ListFineTunes(CancellationToken.None), Times.Exactly(2));
+    }
+}
