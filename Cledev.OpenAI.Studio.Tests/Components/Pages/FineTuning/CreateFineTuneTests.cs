18d30a5df9954a3acdfcd9a0390a1137ec5dae72
lucabriguglia <luca.briguglia@outlook.com>
2023-03-18 13:37:21 +0000

    Add test for fine-tune creation completion

--- Cledev.OpenAI.Studio.Tests/Components/Pages/FineTuning/CreateFineTuneTests.cs
+++ Cledev.OpenAI.Studio.Tests/Components/Pages/FineTuning/CreateFineTuneTests.cs
@@ -18,7 +18,7 @@ public class CreateFineTuneTests : ComponentTestBase
 
         cut.WaitForState(() => cut.Find("div[id=CreateFineTuneModal]") is { TextContent: not null });
 
-        cut.Find("div[id=CreateFineTuneModal]").TextContent.Should().NotBeNull();
+        cut.Find("div[id=CreateFineTuneModal]").ClassList.Contains("show");
     }
 
     [Test]
@@ -39,5 +39,32 @@ public class CreateFineTuneTests : ComponentTestBase
         cut.WaitForState(() => cut.Find("span[id=errorMessage]") is { TextContent: not null });
 
         cut.Find("span[id=errorMessage]").TextContent.Should().Contain("Some Error Message");
+    }
+
+    [Test]
+    public void GivenSubmitFineTuneButtonClicked_WhenAPIReturnsAValidResponse_ThenModalIsClosedAndAPIIsCalledToGetNewList()
+    {
+        OpenAIClient
+            .Setup(x => x.CreateFineTune(It.IsAny<CreateFineTuneRequest>(), CancellationToken.None))
+            .ReturnsAsync(new FineTuneResponse { Id = "NewFineTuneId" });
+
+        OpenAIClient
+            .Setup(x => x.ListFineTunes(CancellationToken.None))
+            .ReturnsAsync(new ListFineTunesResponse { Data = new List<FineTuneResponse> { new() { Id = "Id1", Model = "BaseModel", FineTunedModel = "FineTunedModel" } } });
+
+        JSInterop.SetupVoid("toggleModal", "CreateFineTuneModal");
+
+        var cut = RenderComponent<Studio.Pages.FineTuning>();
+
+        cut.Find("button[id=OpenCreateFineTuneModal]").Click();
+
+        cut.WaitForState(() => cut.Find("div[id=CreateFineTuneModal]") is { TextContent: not null });
+
+        cut.Find("button[id=CreateFineTune]").Click();
+
+        cut.WaitForState(() => true, TimeSpan.FromSeconds(5));
+
+        cut.Find("div[id=CreateFineTuneModal]").ClassList.Contains("hide");
+        OpenAIClient.Verify(client => client.ListFineTunes(CancellationToken.None));
     }
 }
